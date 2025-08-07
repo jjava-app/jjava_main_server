47a676ffeeb0244178150c50eba68e1fa2582c53
Sxias <a32176740@gmail.com>
2025-08-07 14:45:52 +0900

    workspace create & authutil

--- src/main/java/org/example/jjava_main/controller/WorkspaceController.java
+++ src/main/java/org/example/jjava_main/controller/WorkspaceController.java
@@ -1,9 +1,10 @@
 package org.example.jjava_main.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.jjava_main._core.util.Resp;
 import org.example.jjava_main.domain.workspace.WorkspaceService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/workspace")
@@ -11,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkspaceController {
     private final WorkspaceService workspaceService;
 
+    @PostMapping("/create")
+    public ResponseEntity<?> workspaceCreate() {
+        String resp = workspaceService.workspaceCreate();
+        return Resp.ok(resp);
+    }
 }
