0ff8015da6ccfc3b7f0d6c28d47c7cad9180054d
parangdajavous <nuit0535@gmail.com>
2025-08-08 13:02:33 +0900

    [Feat] 워크스페이스 코드 원격 컴파일 (#8)
    * compile
    
    * compile
    
    * 문서 삭제
    
    * 수정

---
+++ src/main/java/org/example/jjava_main/_core/util/HttpUtil.java
@@ -0,0 +1,63 @@
+package org.example.jjava_main._core.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.jjava_main.dto.CompileRequest;
+import org.example.jjava_main.dto.CompileResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Component
+public class HttpUtil {
+
+    private final WebClient webClient;
+
+    // 컴파일 서버 주소
+    private static final String BASE_URL = "http://localhost:8081"; // 컴파일 서버 주소
+    private static final String COMPILE_ENDPOINT = "/compile";
+
+    public HttpUtil() {
+        this.webClient = WebClient.builder()
+                .baseUrl(BASE_URL)
+                .build();
+    }
+
+    /**
+     * 프론트에서 받은 코드를 컴파일 서버로 전송
+     * <p>
+     * reqDTO: 프론트에서 받은 요청(JSON)
+     * return: 컴파일 서버 응답(JSON String)
+     */
+    public CompileResponse.DTO compileServerSend(CompileRequest.DTO reqDTO, Integer userId) {
+        try {
+            Resp<CompileResponse.DTO> resp = webClient.post()
+                    .uri(COMPILE_ENDPOINT)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(reqDTO)
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<Resp<CompileResponse.DTO>>() {
+                    }) // JSON 문자열로 받음 (Resp<CompileResponse.DTO>로 매핑)
+                    .block(); // 동기 방식으로 결과 반환
+
+            if (resp == null) {
+                throw new RuntimeException("컴파일 서버 응답이 없습니다.");
+            }
+
+            if (resp.getStatus() != 200) {
+                // 실패 응답이지만 통신은 정상 → 프론트로 그대로 전달
+                return new CompileResponse.DTO(userId, null, resp.getMsg());
+            }
+
+            // 컴파일 서버 응답에 userId를 추가
+            CompileResponse.DTO respDTO = resp.getBody();
+            respDTO.setUserId(userId);  // 응답에 userId 세팅
+
+            return respDTO;
+        } catch (Exception e) {
+            log.error("컴파일 서버 요청 실패: {}", e.getMessage(), e);
+            throw new RuntimeException("컴파일 서버와 통신 중 오류 발생");
+        }
+    }
+}
