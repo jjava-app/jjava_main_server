8c8244a7eb0e63a0c10759eb7c8f901c89e18685
Sxias <a32176740@gmail.com>
2025-08-12 12:22:08 +0900

    redis 수정

--- src/test/java/org/example/jjava_main/redis/RedisTest.java
+++ src/test/java/org/example/jjava_main/redis/RedisTest.java
@@ -1,14 +1,18 @@
 package org.example.jjava_main.redis;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 public class RedisTest {
     @Autowired
     private StringRedisTemplate redisTemplate;
+
 
     @Test
     public void testRedis() {
