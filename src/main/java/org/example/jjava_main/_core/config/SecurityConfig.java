7f8e61ec8ed9c4fcfaeb40c4b52abbbe91460fda
parangdajavous <nuit0535@gmail.com>
2025-08-18 15:11:56 +0900

    [Fix] 컴파일, 문제 검산 로직 수정
    WebClient -> RestTemplate로 수정
    ㄴ 동기적인 서버에 비동기가 들어가면 제대로 동작 안 하는 문제
    ㄴ WebClient 는 비동기
    
    SecurityConfig
    ㄴ 인증 필요한 uri 추가

--- src/main/java/org/example/jjava_main/_core/config/SecurityConfig.java
+++ src/main/java/org/example/jjava_main/_core/config/SecurityConfig.java
@@ -68,6 +68,11 @@ public class SecurityConfig {
                         .requestMatchers("/").authenticated()
                         .requestMatchers("/users/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/workspace/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/leaderboard/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/compile/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/check/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/questions/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/solved-questions/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
         );
